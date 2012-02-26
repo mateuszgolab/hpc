a9fd4831244c95ad64d15d15ddb90b62d45fe840
mathgol@gmail.com <mathgol@gmail.com@d4767b15-e713-6a93-fd9f-0729faa6068c>
2012-02-26 17:53:31 +0000

    + halo nodes exchange unit tests

--- trunk/HPC/Test/mpiMock.cpp
+++ trunk/HPC/Test/mpiMock.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 
 
-void mpiMock::exchangeHaloNodesMock(matrix & m, int rank, int size, double* upHaloNode, double* downHaloNode)
+void mpiMock::exchangeHaloNodesMock(matrix & m, int rank, int size)
 {
 	if(rank > 0)
 	{
 		// sending second row
 		MPI_Send_Mock(m.getSecondRow(), m.getNumberOfColumns(), rank - 1, rank);
 		// receiving first row
-		MPI_Recv_Mock(m.getFirstRow(), m.getNumberOfColumns(), rank - 1, rank - 1);
+		MPI_Recv_Mock(m.getFirstRow(), m.getNumberOfColumns(), rank - 1, rank);
 	}
 
 	if(rank < size - 1)
@@ -19,19 +19,22 @@ void mpiMock::exchangeHaloNodesMock(matrix & m, int rank, int size, double* upHa
 		// sending penultimate row
 		MPI_Send_Mock(m.getPenultimateRow(), m.getNumberOfColumns(), rank + 1, rank);
 		// receiving last row
-		MPI_Recv_Mock(downHaloNode, m.getNumberOfColumns(), rank + 1, rank + 1);
+		MPI_Recv_Mock(m.getLastRow(), m.getNumberOfColumns(), rank + 1, rank);
 	}
 }
 
 void  mpiMock::MPI_Send_Mock(double* data, int size, int rank , int tag)
 {
-	//haloNodes.insert(pair<int, double*>(rank, data));
+	haloNodes.insert(pair<int, double*>(tag, data));
 }
 
 void  mpiMock::MPI_Recv_Mock(double*  data, int size, int rank , int tag)
 {
 	double *tmp = haloNodes[rank];
-	for(int i = 0; i < size; i++) data[i] = tmp[i];
+	if(tmp != NULL)
+	{
+		for(int i = 0; i < size; i++) data[i] = tmp[i];
+	}
 }
 
 void mpiMock::addHaloNode(int rank, double* haloNode)
--- trunk/HPC/Test/mpiMock.h
+++ trunk/HPC/Test/mpiMock.h
@@ -11,7 +11,7 @@ class mpiMock
 public:
 	void MPI_Send_Mock(double* data, int size, int rank , int tag);
 	void MPI_Recv_Mock(double* data, int size, int rank , int tag);
-	void exchangeHaloNodesMock(matrix & m, int rank, int size, double* up, double* down);
+	void exchangeHaloNodesMock(matrix & m, int rank, int size);
 	void addHaloNode(int rank, double* haloNode);
 };
 
